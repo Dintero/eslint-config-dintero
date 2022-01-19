140e52681f59c1a2ab49f5e5da8fd42d3dcf14d0
muddahany <moh94hany@gmail.com>
2022-01-19 15:56:18 +0200

    fix: include semi colons and spacing for functions in TS

--- typescript.js
+++ typescript.js
@@ -1,23 +1,19 @@
-var { rules } = require('./default')
+var { rules } = require("./default");
 
 module.exports = {
     parser: "@typescript-eslint/parser",
-    extends: [
-        "plugin:@typescript-eslint/recommended"
-    ],
-    plugins: [
-        "@typescript-eslint"
-    ],
+    extends: ["plugin:@typescript-eslint/recommended"],
+    plugins: ["@typescript-eslint"],
     parserOptions: {
         ecmaVersion: 2018,
         sourceType: "module",
         ecmaFeatures: {
-            jsx: false
-        }
+            jsx: false,
+        },
     },
     env: {
         node: true,
-        es6: true
+        es6: true,
     },
     rules: {
         ...rules,
@@ -25,19 +21,43 @@ module.exports = {
         "no-unused-vars": "off",
 
         "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-var-requires": 0,
+        "space-before-function-paren": 0,
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-parameter-properties": "off",
         "@typescript-eslint/interface-name-prefix": "off",
         "@typescript-eslint/consistent-type-definitions": "off",
-        "@typescript-eslint/explicit-module-boundary-types":  "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
         "@typescript-eslint/ban-ts-comment": "off",
         "@typescript-eslint/no-use-before-define": "off",
         "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/no-unused-vars": ["warn", { "args": "none",  "ignoreRestSiblings": true }],
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            { args: "none", ignoreRestSiblings: true },
+        ],
 
-        "@typescript-eslint/member-delimiter-style": ["error", { "multiline": { "delimiter": "none" }, "singleline": { "delimiter": "comma" } }],
-        "@typescript-eslint/type-annotation-spacing": ["error", { "before": false, "after": true, "overrides": { "arrow": { "before": true, "after": true }} }],
+        "@typescript-eslint/member-delimiter-style": [
+            "warn",
+            {
+                multiline: {
+                    delimiter: "semi",
+                    requireLast: true,
+                },
+                singleline: {
+                    delimiter: "semi",
+                    requireLast: false,
+                },
+            },
+        ],
+        "@typescript-eslint/type-annotation-spacing": [
+            "error",
+            {
+                before: false,
+                after: true,
+                overrides: { arrow: { before: true, after: true } },
+            },
+        ],
 
         "@typescript-eslint/indent": "off",
-    }
-}
+    },
+};
